726694e40203e27a5d76a5fd2b30067e23842b0b
ABCD <ABCtoDev@gmail.com>
2023-12-31 18:36:54 +0900

    init project

---
+++ src/test/java/profile/introduce/myself/MyselfApplicationTests.java
@@ -0,0 +1,13 @@
+package profile.introduce.myself;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyselfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
