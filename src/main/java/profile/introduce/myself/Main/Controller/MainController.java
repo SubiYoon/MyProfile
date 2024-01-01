d0d92f907eeb9fd3b010db240564da4849d514dc
ABCD <ABCtoDev@gmail.com>
2024-01-01 20:40:58 +0900

    [feat] template 설정 [fix] pom.xml, gitignore, port 설정 변경

---
+++ src/main/java/profile/introduce/myself/Main/Controller/MainController.java
@@ -0,0 +1,13 @@
+package profile.introduce.myself.Main.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("")
+    public String mainPage(){
+        return "main";
+    }
+}
