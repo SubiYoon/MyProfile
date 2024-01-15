61c0a526ddd93086354d139fccde128149c09677
ABCD <ABCtoDev@gmail.com>
2024-01-15 10:07:46 +0900

    [create] .env, .envproduction 파일 생성 [fix] vue 환경 및 DDL 수정, axios 추가

--- DDL/DDL.sql
+++ DDL/DDL.sql
@@ -1,7 +1,11 @@
-create table yoon_dong_sup
+create schema introduce;
+
+alter schema introduce owner to root;
+
+create table introduce.my_profile
 (
     name                    varchar not null
-        constraint yoon_dong_sup_pk
+        constraint my_profile_pk
             primary key,
     birth_year              integer,
     sex                     varchar,
@@ -14,99 +18,99 @@ create table yoon_dong_sup
     update_ts               date
 );
 
-comment on table yoon_dong_sup is '윤동섭';
+comment on table introduce.my_profile is '프로필';
 
-comment on column yoon_dong_sup.name is '이름';
+comment on column introduce.my_profile.name is '이름';
 
-comment on column yoon_dong_sup.birth_year is '출생년도';
+comment on column introduce.my_profile.birth_year is '출생년도';
 
-comment on column yoon_dong_sup.sex is '성별';
+comment on column introduce.my_profile.sex is '성별';
 
-comment on column yoon_dong_sup.addr is '주소';
+comment on column introduce.my_profile.addr is '주소';
 
-comment on column yoon_dong_sup.addr_detail is '주소상세';
+comment on column introduce.my_profile.addr_detail is '주소상세';
 
-comment on column yoon_dong_sup.simple_introduce_myself is '한줄자기소개';
+comment on column introduce.my_profile.simple_introduce_myself is '한줄자기소개';
 
-comment on column yoon_dong_sup.detail_introduce_myself is '상세자기소개';
+comment on column introduce.my_profile.detail_introduce_myself is '상세자기소개';
 
-comment on column yoon_dong_sup.image is '증명사진';
+comment on column introduce.my_profile.image is '증명사진';
 
-alter table yoon_dong_sup
+alter table introduce.my_profile
     owner to root;
 
 
 -- 경력
-create table abcd.career
+create table introduce.career
 (
-    carrer_seq         integer not null
+    carrer_seq   integer not null
         constraint career_pk
             primary key,
-    company     varchar not null,
-    company_url varchar,
+    company      varchar not null,
+    company_url  varchar,
     company_logo varchar,
-    "in"        date    not null,
-    in_level    varchar not null,
-    out         date,
-    out_level   varchar,
-    regist_ts   date default now(),
-    update_ts   date
+    "in"         date    not null,
+    in_level     varchar not null,
+    out          date,
+    out_level    varchar,
+    regist_ts    date default now(),
+    update_ts    date
 );
 
-comment on table abcd.career is '경력';
+comment on table introduce.career is '경력';
 
-comment on column abcd.career.carrer_seq is '일련번호';
+comment on column introduce.career.carrer_seq is '일련번호';
 
-comment on column abcd.career.company is '회사명';
+comment on column introduce.career.company is '회사명';
 
-comment on column abcd.career.company_url is '회사 홈페이지';
+comment on column introduce.career.company_url is '회사 홈페이지';
 
-comment on column abcd.career.company_logo is '회사_로고';
+comment on column introduce.career.company_logo is '회사_로고';
 
-comment on column abcd.career."in" is '입사년월일';
+comment on column introduce.career."in" is '입사년월일';
 
-comment on column abcd.career.in_level is '입사 직책';
+comment on column introduce.career.in_level is '입사 직책';
 
-comment on column abcd.career.out is '퇴사년월일';
+comment on column introduce.career.out is '퇴사년월일';
 
-comment on column abcd.career.out_level is '퇴사 직전 직책';
+comment on column introduce.career.out_level is '퇴사 직전 직책';
 
-comment on column abcd.career.update_ts is '수정';
+comment on column introduce.career.update_ts is '수정';
 
-alter table abcd.career
+alter table introduce.career
     owner to root;
 
 -- 프로젝트
-create table abcd.project
+create table introduce.project
 (
-    project_seq  integer not null
+    project_seq        integer not null
         constraint project_pk
             primary key,
-    career_seq   integer not null,
-    project_name varchar,
-    project_term integer,
+    career_seq         integer not null,
+    project_name       varchar,
+    project_term       integer,
     project_contribute varchar,
-    regist_ts   date default now(),
-    update_ts   date
+    regist_ts          date default now(),
+    update_ts          date
 );
 
-comment on table abcd.project is '프로젝트';
+comment on table introduce.project is '프로젝트';
 
-comment on column abcd.project.career_seq is '경력_일련번호';
+comment on column introduce.project.career_seq is '경력_일련번호';
 
-comment on column abcd.project.project_seq is '일련번호';
+comment on column introduce.project.project_seq is '일련번호';
 
-comment on column abcd.project.project_name is '프로젝트_이름';
+comment on column introduce.project.project_name is '프로젝트_이름';
 
-comment on column abcd.project.project_term is '프로젝트_기간';
+comment on column introduce.project.project_term is '프로젝트_기간';
 
-comment on column abcd.project.project_contribute is '프로젝트_기여도';
+comment on column introduce.project.project_contribute is '프로젝트_기여도';
 
-alter table abcd.project
+alter table introduce.project
     owner to root;
 
 -- 프로젝트 상세
-create table abcd.project_detail
+create table introduce.project_detail
 (
     project_detail_seq integer not null
         constraint project_detail_pk
@@ -115,27 +119,27 @@ create table abcd.project_detail
     detail_act_title   varchar,
     detail_act_cont    varchar,
     detail_act_term    integer,
-    regist_ts   date default now(),
-    update_ts   date
+    regist_ts          date default now(),
+    update_ts          date
 );
 
-comment on table abcd.project_detail is '프로젝트_상세';
+comment on table introduce.project_detail is '프로젝트_상세';
 
-comment on column abcd.project_detail.project_detail_seq is '프로젝트_상세_일련번호';
+comment on column introduce.project_detail.project_detail_seq is '프로젝트_상세_일련번호';
 
-comment on column abcd.project_detail.project_seq is '프로젝트_일련번호';
+comment on column introduce.project_detail.project_seq is '프로젝트_일련번호';
 
-comment on column abcd.project_detail.detail_act_title is '상세_실시_항목';
+comment on column introduce.project_detail.detail_act_title is '상세_실시_항목';
 
-comment on column abcd.project_detail.detail_act_cont is '상세_실시_내용';
+comment on column introduce.project_detail.detail_act_cont is '상세_실시_내용';
 
-comment on column abcd.project_detail.detail_act_term is '상세_실시_기간';
+comment on column introduce.project_detail.detail_act_term is '상세_실시_기간';
 
-alter table abcd.project_detail
+alter table introduce.project_detail
     owner to root;
 
 -- 프로젝트 스택
-create table abcd.project_stack
+create table introduce.project_stack
 (
     project_stack_seq integer not null
         constraint project_stack_pk
@@ -143,47 +147,52 @@ create table abcd.project_stack
     project_seq       integer,
     stack_kind        varchar,
     stack_img         varchar,
+    regist_ts         date default now(),
+    update_ts         date
+);
+
+comment on table introduce.project_stack is '프로젝트 스택';
+
+comment on column introduce.project_stack.project_stack_seq is '프로젝트_스택_일련번호';
+
+comment on column introduce.project_stack.project_seq is '프로젝트_일련번호';
+
+comment on column introduce.project_stack.stack_kind is '스택_종류';
+
+comment on column introduce.project_stack.stack_img is '스택_이미지';
+
+alter table introduce.project_stack
+    owner to root;
+
+-- 내가 사용 가능한 스택
+create table introduce.my_stack
+(
+    stack_seq   integer not null
+        constraint my_stack_pk
+            primary key,
+    stack_name  varchar,
+    stack_level varchar,
+    stack_img   varchar,
     regist_ts   date default now(),
     update_ts   date
 );
 
-comment on table abcd.project_stack is '프로젝트 스택';
+comment on table introduce.my_stack is '나의_스택';
 
-comment on column abcd.project_stack.project_stack_seq is '프로젝트_스택_일련번호';
+comment on column introduce.my_stack.stack_seq is '스택_일련번호';
 
-comment on column abcd.project_stack.project_seq is '프로젝트_일련번호';
+comment on column introduce.my_stack.stack_name is '스택_이름';
 
-comment on column abcd.project_stack.stack_kind is '스택_종류';
+comment on column introduce.my_stack.stack_level is '스택_숙련도';
 
-comment on column abcd.project_stack.stack_img is '스택_이미지';
+comment on column introduce.my_stack.stack_img is '스택_이미지';
 
-alter table abcd.project_stack
-    owner to root;
-
--- 내가 사용 가능한 스택
-create table abcd.my_stack
-(
-    stack_seq integer not null
-        constraint my_stack_pk
-            primary key,
-    stack_name varchar,
-    stack_level varchar,
-    stack_img varchar,
-    regist_ts date default now(),
-    update_ts date
-);
-
-comment on table abcd.my_stack is '나의_스택';
-
-comment on column abcd.my_stack.stack_seq is '스택_일련번호';
-
-comment on column abcd.my_stack.stack_name is '스택_이름';
-
-comment on column abcd.my_stack.stack_level is '스택_숙련도';
-
-comment on column abcd.my_stack.stack_img is '스택_이미지';
-
-alter table abcd.my_stack
+alter table introduce.my_stack
     owner to root;
 
 
+-- 자신의 정보를 입력하고 쿼리를 날리세요.(간단한 소개, 상세 소개, 증명사진자리 세가지는 나중에 기능 생성후 삽입예정 작성 안하셔도 됩니다.)
+INSERT INTO introduce.my_profile
+(name, birth_year, sex, addr, addr_detail, simple_introduce_myself, detail_introduce_myself, image, regist_ts, update_ts)
+VALUES ('홍길동', 2024, '남자', '충북 청주시 흥덕구 오송읍', '오송1길 165-7', '간단 소개', '상세 소개', '증명사진', now(), now());
+commit;
--- front/src/components/HelloWorld.vue
+++ front/src/components/HelloWorld.vue
@@ -5,6 +5,7 @@
             For a guide and recipes on how to configure / customize this project,<br />
             check out the
             <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+            <button @click="test">콘솔테스트버튼</button>
         </p>
         <h3>Installed CLI Plugins</h3>
         <ul>
@@ -31,10 +32,21 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
     name: 'HelloWorld',
     props: {
         msg: String,
+    },
+    setup() {
+        function test() {
+            axios.post('main').then(() => {
+                console.log('요청완료')
+            })
+        }
+
+        return { test }
     },
 }
 </script>
@@ -44,14 +56,17 @@ export default {
 h3 {
     margin: 40px 0 0;
 }
+
 ul {
     list-style-type: none;
     padding: 0;
 }
+
 li {
     display: inline-block;
     margin: 0 10px;
 }
+
 a {
     color: #42b983;
 }
