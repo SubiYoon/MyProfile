1efb7a549603b5e75ccfb1366476fcf75cf1d118
ecosangmin <xectler2@gmail.com>
2024-01-19 17:00:27 +0900

    필요 없는것 정리 및 tailwind setup

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,9 +1,5 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup></script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <main class="text-red-500">tailwind-test</main>
 </template>
