205a83bffe3aa45f601bd9907016f54768dc0d4a
ABCD <ABCtoDev@gmail.com>
2024-01-26 10:40:27 +0900

    [fix] cors정책 변경

--- src/main/java/profile/introduce/myself/config/SecurityConfig.java
+++ src/main/java/profile/introduce/myself/config/SecurityConfig.java
@@ -10,12 +10,12 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
 import java.util.Collections;
 
 @Configuration
@@ -24,31 +24,49 @@ import java.util.Collections;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    @Bean
-    public static BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
+    /**
+     * 정적 자원에 대해 보안을 적용하지 않도록 설정
+     */
     @Bean
     WebSecurityCustomizer webSecurityCustomizer() {
         return (web) -> web.ignoring()
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 
+    /**
+     * Spring Security 설정
+     * <ul>
+     *     <li>csrf 방지 기능 비활성화</li>
+     *     <li>`/admin/**` 페이지만 인증 필요하게 설정</li>
+     *     <li>로그인 성공시 `/`로 이동</li>
+     *     <li>로그아웃시 세션삭제 및 쿠키삭제</li>
+     * </ul>
+     */
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
+                //인증 인가가 필요한 URL을 지정
                 .authorizeHttpRequests(requests -> requests
-                        .requestMatchers( "/**").permitAll()
-                        .anyRequest().authenticated()
+                        //특정 패턴의 URL 인증이 필요함을 표시(authenticated())
+                        .requestMatchers( "/admin/**").authenticated()
+                        //나머지 요청은 전부 허용
+                        .anyRequest().permitAll()
                 )
+                //로그인 폼 방식 적용
                 .formLogin(form -> form
+                        //로그인 폼 URL
                         .loginPage("/login")
+                        //성공시 "/"페이지로 이동
+                        .loginProcessingUrl("/profile/login")
                         .defaultSuccessUrl("/", true)
+                        //인증 인가 필요없이 허용
                         .permitAll()
                 )
-                .logout(LogoutConfigurer::permitAll);
+                //로그아웃에 대한 정보
+                .logout()
+                .logoutUrl("/profile/logout")
+                .invalidateHttpSession(true).deleteCookies("JSESSIONID");
 
         return httpSecurity.build();
     }
@@ -56,13 +74,18 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
+        // 쿠키 or 인증토큰을 포함하는 요청을 승인여부
         config.setAllowCredentials(true);
+        // 요청 가능한 도메인 설정
         config.setAllowedOrigins(Collections.singletonList("*"));
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
+        // 요청시 받고을 수 있는 헤더 설정
+        config.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization", "Content-Length", "X-Requested-Width", "X-XSRF-token"));
+        // 요청시 가능한 Method 서정
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        config.setMaxAge(3600L);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/api/*", config);
+        source.registerCorsConfiguration("/api/**", config);
 
         return source;
     }
