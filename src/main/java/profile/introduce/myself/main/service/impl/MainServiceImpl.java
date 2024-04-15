5d8da6f351ee806981f0ede688b1d3e9a85ed759
park74441 <118245133+park74441@users.noreply.github.com>
2024-04-15 19:42:17 +0900

    feat.2페이지 프로필 사진 추가(디테일 추가 예정)

--- src/main/java/profile/introduce/myself/main/service/impl/MainServiceImpl.java
+++ src/main/java/profile/introduce/myself/main/service/impl/MainServiceImpl.java
@@ -3,6 +3,7 @@ package profile.introduce.myself.main.service.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import profile.introduce.myself.main.vo.MenuVo;
+import profile.introduce.myself.main.vo.ProfileVo;
 import profile.introduce.myself.main.vo.SampleVo;
 import profile.introduce.myself.main.mapper.MainMapper;
 import profile.introduce.myself.main.service.MainService;
@@ -23,5 +24,10 @@ public class MainServiceImpl implements MainService {
     @Override
     public List<MenuVo> selectMenu() throws Exception {
         return mainMapper.selectMenu();
+    }
+
+    @Override
+    public List<ProfileVo> selectProfile() throws Exception {
+        return mainMapper.selectProfile();
     }
 }
---
+++ src/main/java/profile/introduce/myself/main/vo/ProfileVo.java
@@ -0,0 +1,19 @@
+package profile.introduce.myself.main.vo;
+
+import lombok.Data;
+
+@Data
+public class ProfileVo {
+    private String name;
+    private String password;
+    private String birthYear;
+    private String sex;
+    private String addr;
+    private String addrDetail;
+    private String simpleIntroduceMyself;
+    private String detailIntroduceMyself;
+    private String image;
+    private String registTs;
+    private String updateTs;
+
+}
