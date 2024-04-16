2d39cb2627ba1587a6834c4af4f53dd6d8fca194
ABCD <ABCtoDev@gmail.com>
2024-04-17 00:30:01 +0900

    [Feat] UserController.java :: log 작성

--- src/main/java/profile/introduce/myself/user/controller/UserController.java
+++ src/main/java/profile/introduce/myself/user/controller/UserController.java
@@ -1,11 +1,12 @@
 package profile.introduce.myself.user.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import profile.introduce.myself.user.service.UserService;
-import profile.introduce.myself.user.vo.ProfileVo;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -16,6 +17,8 @@ public class UserController {
     @Autowired
     UserService userService;
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
+
     @RequestMapping("/name/{name}")
     Map<String, Object> chooseProfile(@PathVariable("name")  String name){
         String realName = "";
@@ -24,6 +27,8 @@ public class UserController {
         } else if("parkjs".equals(name)){
             realName = "박지수";
         }
+
+        LOGGER.info("유저 조회 :: " + realName + " 조회");
 
         Map<String, Object> result = new HashMap<>();
 
