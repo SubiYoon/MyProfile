f3de6310b9a8f195c9a33f2e890f41aed984fce6
ABCD <ABCtoDev@gmail.com>
2024-04-18 17:26:46 +0900

    [Refact] StackList :: List 형태로 변경 및 메서드명 변경

--- src/main/java/profile/introduce/myself/user/service/UserService.java
+++ src/main/java/profile/introduce/myself/user/service/UserService.java
@@ -3,8 +3,6 @@ package profile.introduce.myself.user.service;
 import profile.introduce.myself.user.vo.MyStackVo;
 import profile.introduce.myself.user.vo.ProfileVo;
 
-import java.util.List;
-
 public interface UserService {
     ProfileVo chooseProfile(String alias);
     List<MyStackVo> getStackList(String name);
