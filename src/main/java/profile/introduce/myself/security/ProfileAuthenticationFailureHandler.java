12a02800604ccb66455ff39d9c6a08dc59fc67ef
ABCD <ABCtoDev@gmail.com>
2024-04-27 21:55:42 +0900

    [Feat] Login :: 로그인 구현중

---
+++ src/main/java/profile/introduce/myself/security/ProfileAuthenticationFailureHandler.java
@@ -0,0 +1,31 @@
+package profile.introduce.myself.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class ProfileAuthenticationFailureHandler implements AuthenticationFailureHandler {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        String errMsg = "이름 또는 비밀번호가 틀렸습니다.";
+
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+        objectMapper.writeValue(response.getWriter(), errMsg);
+    }
+}
