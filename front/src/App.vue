d70c22f577c9e3a1a41dd5e2e6062398bfbfef1c
ABCD <ABCtoDev@gmail.com>
2024-04-27 21:55:33 +0900

    [Feat] Login :: 로그인 구현중

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,19 +1,7 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import { instance } from '@/api'
 
 const mode = import.meta.env.MODE
-
-async function test() {
-    try {
-        const res = await instance.get('/api/menu')
-        console.log(res)
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-test()
 </script>
 
 <template>
