b9c5debea8881054e73f154555bb37cdca50a667
ABCD <ABCtoDev@gmail.com>
2024-04-29 16:01:10 +0900

    [Refact] UserController.java :: 유저 데이터 없을때 예외 처리

--- src/main/java/profile/introduce/myself/user/controller/UserController.java
+++ src/main/java/profile/introduce/myself/user/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import profile.introduce.myself.user.service.UserService;
 import profile.introduce.myself.user.vo.ProfileVo;
+import profile.introduce.myself.utility.ItemCheck;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -23,11 +24,17 @@ public class UserController {
     @RequestMapping("/name/{name}")
     Map<String, Object> chooseProfile(@PathVariable("name")  String alias){
 
+        Map<String, Object> result = new HashMap<>();
+
         ProfileVo userProfile = userService.chooseProfile(alias);
 
-        LOGGER.info("유저 조회 :: " + userProfile.getName() + " 조회");
+        if(ItemCheck.isEmpty(userProfile)){
+            result.put("user", "User Not Found!!");
 
-        Map<String, Object> result = new HashMap<>();
+            return result;
+        }
+
+        LOGGER.info("유저 조회 :: " + userProfile.getName() + " 조회");
 
         result.put("profile", userProfile);
         result.put("stack", userService.getStackList(userProfile.getName()));
