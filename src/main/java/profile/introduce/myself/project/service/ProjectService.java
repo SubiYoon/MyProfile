e0df56b3fa79a0fe920e6282e421938dc802cc7d
ABCD <ABCtoDev@gmail.com>
2024-05-04 22:49:37 +0900

    [Feat] Project :: file setting

---
+++ src/main/java/profile/introduce/myself/project/service/ProjectService.java
@@ -0,0 +1,4 @@
+package profile.introduce.myself.project.service;
+
+public interface ProjectService {
+}
---
+++ src/main/java/profile/introduce/myself/project/service/impl/ProjectServiceImpl.java
@@ -0,0 +1,7 @@
+package profile.introduce.myself.project.service.impl;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectServiceImpl {
+}
