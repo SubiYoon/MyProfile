dddc94f349fef6d5691b59955ca327778f7a024c
park74441 <118245133+park74441@users.noreply.github.com>
2024-05-22 13:04:54 +0900

    [Refact] 로그인 화면 수정

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -24,32 +24,42 @@ function login() {
 </script>
 
 <template>
-    <q-card class="login-card">
-        <q-card-section>
-            <div class="text-h6">Login</div>
-        </q-card-section>
+    <div class="login-form">
+        <q-card class="login-card">
+            <q-card-section>
+                <div class="text-h6">Login</div>
+            </q-card-section>
 
-        <q-card-section>
-            <q-form @submit="login">
-                <q-input v-model="id" filled label="닉네임" lazy-rules :rules="[val => !!val || '닉네임을 입력해주세요.']" />
-                <q-input
-                    v-model="pwd"
-                    filled
-                    type="password"
-                    label="비밀번호"
-                    lazy-rules
-                    :rules="[val => !!val || '비밀번호를 입력해주세요']"
-                    @keyup.enter="login"
-                />
-                <q-btn type="submit" label="로그인" color="primary" class="full-width q-mt-md" />
-            </q-form>
-            <q-btn @click="regist" label="회원가입" color="primary" class="full-width q-mt-md" />
-        </q-card-section>
-    </q-card>
+            <q-card-section>
+                <q-form @submit="login">
+                    <q-input v-model="id" filled label="닉네임" lazy-rules :rules="[val => !!val || '닉네임을 입력해주세요.']" />
+                    <q-input
+                        v-model="pwd"
+                        filled
+                        type="password"
+                        label="비밀번호"
+                        lazy-rules
+                        :rules="[val => !!val || '비밀번호를 입력해주세요']"
+                        @keyup.enter="login"
+                    />
+                    <q-btn type="submit" label="로그인" color="primary" class="full-width q-mt-md" />
+                </q-form>
+                <q-btn @click="regist" label="회원가입" color="primary" class="full-width q-mt-md" />
+            </q-card-section>
+        </q-card>
+    </div>
 </template>
 
 <style scoped>
 .login-card {
     width: 400px;
+}
+
+.login-form {
+    width: 100%;
+    height: 100%;
+    align-items: center;
+    justify-content: center;
+    display: flex;
 }
 </style>
