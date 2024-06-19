d2e22e28210768f9e1ceb44e7e994d39329fd098
park74441 <118245133+park74441@users.noreply.github.com>
2024-06-19 18:42:00 +0900

    [Fix] 세번째 페이지 크기 변경

--- front2/src/recoil.js
+++ front2/src/recoil.js
@@ -2,31 +2,31 @@ import { atom, useRecoilState } from 'recoil';
 
 // currentPage 상태를 위한 atom 정의
 export const currentPageState = atom({
-    key: 'currentPageState', // 상태를 식별하는 고유 키
-    default: 1, // 초기값 설정
+    key: 'currentPageState', //페이지 상태 업데이트
+    default: 1, // 초기값
 });
 
 export const userState = atom({
-    key: 'userState',
+    key: 'userState', //user 정보 업데이트
     default: null,
 });
 
 export const profileState = atom({
-    key: 'profileState',
+    key: 'profileState', //프로필 정보 업데이트
     default: {},
 });
 
 export const stackState = atom({
-    key: 'stackState',
+    key: 'stackState', //스택 정보 업데이트
     default: [],
 });
 
 export const careerState = atom({
-    key: 'careerState',
+    key: 'careerState', //커리어 정보 업데이트
     default: null,
 });
 
 export const educationState = atom({
-    key: 'educationState',
+    key: 'educationState', //교육이력 정보 업데이트
     default: null,
 });
