fc408afb07928d997c5deb185687d8e97d01beda
parkjisu <park74441@naver.com>
2024-06-24 16:20:43 +0900

    [Fix] 프로젝트 모드 수정 및 소개 html 형식 수정

--- front2/src/pages/Profile.jsx
+++ front2/src/pages/Profile.jsx
@@ -30,6 +30,8 @@ const Profile = React.memo(() => {
     // 스킬 정보
     const stackData = useRecoilValue(stackState);
 
+    console.log('프로필0', profileData);
+
     if (!profileData || !stackData) {
         return <h2>Loading...</h2>;
     }
